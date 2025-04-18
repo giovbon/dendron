da85a12cb6af4e42c8976d49e07812f72f9c2df1
Giovani Bontempo <tempogiobon@gmail.com>
2025-04-18 15:55:06 -0300

    auto

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,4 +1,3 @@
-// ========== COMMON ==========
 const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 const watchTarget = document.getElementById("app-container");
 
@@ -6,34 +5,30 @@ const throttle = (func, limit) => {
   let inThrottle;
   return (...args) => {
     if (!inThrottle) {
-      func(...args);
+      requestAnimationFrame(() => func(...args));
       inThrottle = setTimeout(() => (inThrottle = false), limit);
     }
   };
 };
-// ========== END COMMON ==========
 
-// ========== NAMESPACE PREFIXES COLLAPSER ==========
 function hideNamespace() {
-  console.info("====== LS HIDE NAMESPACE v20220314 =====");
-  let nmsp = document.querySelectorAll(
-    'a.page-ref[data-ref*="/"]:not(.hidden-namespace)'
-  );
-  for (let i = 0; i < nmsp.length; i++) {
-    if (nmsp[i].innerText.indexOf("/") !== -1) {
-      nmsp[i].innerHTML =
-        "<span style='color:rgb(133, 211, 81)'>..</span>" +
-        nmsp[i].innerText.substring(nmsp[i].innerText.lastIndexOf("/"));
-      nmsp[i].classList.add("hidden-namespace");
-    }
-  }
+  document
+    .querySelectorAll('a.page-ref[data-ref*="/"]:not(.hidden-namespace)')
+    .forEach((el) => {
+      const text = el.textContent;
+      if (text.includes("/")) {
+        el.innerHTML =
+          "<span style='color:rgb(133, 211, 81)'>..</span>" +
+          text.substring(text.lastIndexOf("/"));
+        el.classList.add("hidden-namespace");
+      }
+    });
 }
 
 const updateHideNamespace = throttle(hideNamespace, 1000);
 const obsNamespace = new MutationObserver(updateHideNamespace);
 obsNamespace.observe(watchTarget, {
   subtree: true,
-  attributes: true,
+  childList: true,
 });
-// ========== END NAMESPACE PREFIXES COLLAPSER ==========
 
